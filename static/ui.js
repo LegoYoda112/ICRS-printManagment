1451903c18bdf4d8fe3ccd90d50f6df69d4e685c
Thomas G <thomas.godden@outlook.com>
2020-06-07 23:23:42 -0700

    Set up auto update of printer content

--- static/ui.js
+++ static/ui.js
@@ -5,31 +5,63 @@ const UI = Object.create(null);
 const el = (id) => document.getElementById(id);
 const cloneTemplate = (id) => document.importNode(el(id).content, true);
 
+const printerList = el("printer-list");
+const printList = el("print-list");
 
-function updatePrinter(printerParent, printer) {
-    const qs = (query) => printerParent.querySelector(query);
+const printerUpdateSpeed = 5000;
 
-    qs(".printer").id = printer.printer_id;
+function updatePrinter(printerElement, printer) {
+    const qs = (query) => printerElement.querySelector(query);
+
+    if (printer.status !== "OK") {
+        qs(".printer-image").style.filter = "grayscale(100%)";
+    } else {
+        qs(".printer-image").style.filter = "grayscale(0%)";
+    }
+
     qs(".printer-name").textContent = printer.name;
     qs(".status").textContent = printer.status;
     qs(".progress").textContent = printer.progress;
     qs(".remaining-time").textContent = printer.remaining_time;
 }
 
-UI.init = function () {
-
-    const printerList = el("printer-list");
-    const printList = el("print-list");
+function updatePrinters(){
+    const printerElements = document.getElementsByClassName("printer");
 
     // Create list of printers
     database.getPrinters().then(function (printers) {
-        console.log(printers);
         printers.forEach(function (printer) {
-            const newPrinter = cloneTemplate("printer");
-            updatePrinter(newPrinter, printer);
-            printerList.appendChild(newPrinter);
+            let printerExists = false;
+            
+            // If printer element already exist in list
+            // update it
+            for (const printerElement of printerElements) {
+                if(printer.printer_id == printerElement.id){
+                    updatePrinter(printerElement, printer);
+                    printerExists = true;
+                }
+            }
+
+            // If printer element doesn't exist, add it
+            if (!printerExists) {
+                let newPrinter = cloneTemplate("printer");
+                // console.log(newPrinter);
+                updatePrinter(newPrinter, printer);
+                newPrinter.querySelector(".printer").id = printer.printer_id;
+                printerList.appendChild(newPrinter);
+            }
         });
     });
+
+}
+
+UI.init = function () {
+    let printerUpdateID = setTimeout(function update() {
+        updatePrinters();
+        console.log("timer ran");
+
+        printerUpdateID = setTimeout(update, printerUpdateSpeed);
+    }, printerUpdateSpeed);
 
     // database.getLatestPrints().then(function (prints) {
     //     console.log(prints);
